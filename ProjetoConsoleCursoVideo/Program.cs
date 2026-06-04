58f5d1afd396bb9ac7793f627a72e5234a136a09
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-06-04 09:15:49 -0300

    Add project files.

---
+++ ProjetoConsoleCursoVideo/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write(1.2);
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("oi, tudo bem?");
+
+Console.WriteLine(123456);
+
+Console.Write("oiiiiiiiiiii");
+Console.WriteLine("tudo bem");
+
+Console.WriteLine("\tOi tudo bem?");
+
+Console.WriteLine("oi \ntudo bem?");
+
+// isso é um comentario
+
+/*
+ wewe
+edwwe
+dede
+ */
