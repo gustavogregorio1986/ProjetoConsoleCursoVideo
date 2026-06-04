d31c7df99564a1f1f0106b21cd3b75d0df9987e2
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-06-04 16:10:38 -0300

    Desenvolvet calculadora de multiplicação na logica de programação

--- ProjetoConsoleCursoVideo/Program.cs
+++ ProjetoConsoleCursoVideo/Program.cs
@@ -20,29 +20,29 @@ edwwe
 dede
  */
 
-string nome = "Eduardo";
-int idade = 24;
-double altura = 1.67;
-bool vivo = false;
-char letra = 'c';
+//string nome = "Eduardo";
+//int idade = 24;
+//double altura = 1.67;
+//bool vivo = false;
+//char letra = 'c';
 
 
-Console.WriteLine(nome);
-Console.WriteLine(idade);
-Console.WriteLine(altura);
-Console.WriteLine(letra);
-Console.WriteLine(vivo);
+//Console.WriteLine(nome);
+//Console.WriteLine(idade);
+//Console.WriteLine(altura);
+//Console.WriteLine(letra);
+//Console.WriteLine(vivo);
 
 //nom: nome idade: idade
-Console.WriteLine("Nome: " + nome + " Idade: " + idade);
+//Console.WriteLine("Nome: " + nome + " Idade: " + idade);
 
-Console.WriteLine($"Nome:{nome} Idade:{idade}");
+//Console.WriteLine($"Nome:{nome} Idade:{idade}");
 
-nome = "Crislaine Luana";
-idade = 68;
+//nome = "Crislaine Luana";
+//idade = 68;
 
 
-Console.WriteLine($"Nome:{nome} Idade:{idade}");
+//Console.WriteLine($"Nome:{nome} Idade:{idade}");
 
 const double pi = 3.14159;
 
@@ -50,21 +50,21 @@ const double pi = 3.14159;
 
 Console.WriteLine(pi);
 
-double a = 3.14;
-int b = Convert.ToInt32(a);
+//double a = 3.14;
+//int b = Convert.ToInt32(a);
 
-Console.WriteLine(a);
-Console.WriteLine(a.GetType());
-Console.WriteLine(b);
-Console.WriteLine(b.GetType());
+//Console.WriteLine(a);
+//Console.WriteLine(a.GetType());
+//Console.WriteLine(b);
+//Console.WriteLine(b.GetType());
 
-int c = 3;
-double d = Convert.ToDouble(c);
+//int c = 3;
+//double d = Convert.ToDouble(c);
 
-Console.WriteLine(c);
-Console.WriteLine(c.GetType());
-Console.WriteLine(d);
-Console.WriteLine(d.GetType());
+//Console.WriteLine(c);
+//Console.WriteLine(c.GetType());
+//Console.WriteLine(d);
+//Console.WriteLine(d.GetType());
 
 //string a = "2";
 //int b = Convert.ToInt32(a);
@@ -82,6 +82,104 @@ Console.WriteLine(d.GetType());
 //Console.WriteLine(b);
 //Console.WriteLine(b.GetType());
 
+//Console.WriteLine("Qual seu nome?");
+//string nome = Console.ReadLine();
 
+//Console.WriteLine("Qual sua idade?");
+//int idade = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine($"O seu nome é: {nome} e sua idade é {idade} anos!");
+
+//int numero = 1;
+
+
+//numero = numero + 1;
+//Console.WriteLine(numero);
+//numero = numero - 1;
+//Console.WriteLine(numero);
+//numero -= 10;
+//Console.WriteLine(numero);
+//numero--;
+
+//Console.WriteLine(numero);
+
+//numero = numero + 2;
+//Console.WriteLine(numero);
+
+//numero *= 2;
+//Console.WriteLine(numero);
+
+//numero = numero / 2;
+
+//Console.WriteLine(numero);
+
+//numero /= 2;
+//Console.WriteLine(numero);
+
+//int resto;
+
+//int numero = 10;
+
+//resto = numero % 4;
+
+//Console.WriteLine(resto);
+
+//double x = 3;
+
+//double w = -5;
+//double a = Math.Pow(x, 2);
+//double z = 4.5;
+//Console.WriteLine(a);
+
+//double y = 81;
+//double b = Math.Sqrt(y); 
+//Console.WriteLine(b);
+//double c = Math.Abs(b);
+//Console.WriteLine(w);
+//double d = Math.Round(w);
+//Console.WriteLine(z);
+//double e = Math.Ceiling(z);
+//Console.WriteLine(e);
+//double f = Math.Floor(z);
+//Console.WriteLine(f);
+//double g = Math.Max(y, x);
+//Console.WriteLine(g);
+
+//int variavell = 0;
+//Random random = new Random();
+
+//int num = random.Next(1,7);
+//double numDecimal = random.NextDouble();
+
+//Console.WriteLine(num);
+//Console.WriteLine(numDecimal);
+
+//Console.WriteLine("Digite o primeiro numero: ");
+//int numero1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Digite o segundo numero: ");
+//int numero2 = Convert.ToInt32(Console.ReadLine());
+
+//int resultado = numero1 * numero2;
+
+//Console.WriteLine($"O resultado é {resultado}");
+
+//Console.WriteLine("Digite o primeiro nome: ");
+//string nome1 = Console.ReadLine();
+
+//Console.WriteLine("Digite o segundo nome: ");
+//string nome2 = Console.ReadLine();
+
+//Console.WriteLine($"O nome compelto que é {nome1} {nome2}");
+
+//Console.WriteLine("Digite o primeiro numero ");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Digite o segundo numero ");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+
+//int resultado1 = num1 + num2;
+
+//Console.WriteLine($"O resulktado é {resultado}");
 
 Console.ReadKey();
