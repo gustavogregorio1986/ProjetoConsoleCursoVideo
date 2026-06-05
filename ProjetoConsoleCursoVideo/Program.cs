02db5c0d412e1ad0daa285b92b36c8f1b22f723a
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-06-05 19:58:46 -0300

    Desenvolvimento da logica de programação com loops alinhadas

--- ProjetoConsoleCursoVideo/Program.cs
+++ ProjetoConsoleCursoVideo/Program.cs
@@ -364,4 +364,29 @@ for (int index = 10; index >= 0; index--)
     Console.WriteLine(index);
 }
 
+/*
+
+   masted logos 
+
+ */
+
+Console.Write("Quantas linhas?:  ");
+int linhas = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Quantas colunas?:  ");
+int colunas = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("QWWual o simbolo?: ");
+string simbolo = Console.ReadLine();
+
+
+for(int i = 0; i < linhas; i++)
+{
+    for(int j = 0; j < colunas; j++)
+    {
+        Console.Write(simbolo);
+    }
+    Console.WriteLine();
+}
+
 Console.ReadKey();
